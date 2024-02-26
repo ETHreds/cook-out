431dce7740bf4dfd402eeb7712aa007c70009438
ETHreds <jcmaglaw@gmail.com>
2024-02-26 16:47:28 +0300

    reset

--- node-api/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const { createAppMiddleware } = require('./SSRmiddleware');
-
-// Use createAppMiddleware as middleware for the '/' route
-app.get('/', createAppMiddleware);
-
-module.exports = app;
